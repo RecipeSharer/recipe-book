ba443c580638448a6863ecdf6725ea5b5ca47956
Clayton Knapp <csjknapp@gmail.com>
2022-05-19 14:18:51 -0700

    started on EditRecipeView with form

---
+++ src/views/EditRecipeView.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import useRecipes from '../hooks/useRecipes';
+// import useUser from '../hooks/useUser';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default function EditRecipeView() {
+  const { add } = useRecipes();
+  // const { user } = useUser();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [ingredients, setIngredients] = useState(null);
+  const history = useHistory();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const editedRecipe = { title, description, ingredients };
+
+    await add(editedRecipe);
+    history.replace('/recipes');
+  }
+
+  return (
+        <>
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e) => setTitle(e.target.value)} placeholder="title" />
+        <textarea
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="description"
+        />
+        <input
+          onChange={(e) => setIngredients(e.target.value)}
+          placeholder="ingredients"
+        />
+        <button onClick={add}>Add Recipe</button>
+      </form>
+    </>
+  )
+};
