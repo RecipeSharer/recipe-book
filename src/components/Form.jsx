2e86ef9a0364c136022bbddc4ff0e295b2c6c927
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-20 16:42:49 -0700

    images being added to the recipes properly

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,44 +3,46 @@ import { useState } from 'react';
 import toast from 'react-hot-toast';
 import styles from './Form.css';
 
-
 export default function Form({ recipe, onSubmit, label }) {
   const [title, setTitle] = useState(recipe.title || '');
   const [description, setDescription] = useState(recipe.description || '');
-  const [ingredients, setIngredients] = useState(recipe.ingredients || '');; 
+  const [ingredients, setIngredients] = useState(recipe.ingredients || '');
+  const [imgUrl, setImgUrl] = useState(recipe.img_url || '');
 
-  const newRecipe = { title, description, ingredients };
+  const newRecipe = { title, description, ingredients, img_url: imgUrl };
 
   async function handleSubmit(e) {
     e.preventDefault();
     try {
-      await onSubmit(newRecipe)
+      await onSubmit(newRecipe);
     } catch (error) {
       toast.error(error);
     }
   }
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className={styles.form}
-    >
-          <input
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="title"
-          />
-          <textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder="description"
-          />
-          <input
-            value={ingredients}
-            onChange={(e) => setIngredients(e.target.value)}
-            placeholder="ingredients"
-          />
-          <button>{`${label}`} Recipe</button>
-        </form>
-  )
-};
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="title"
+      />
+      <textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="description"
+      />
+      <input
+        value={ingredients}
+        onChange={(e) => setIngredients(e.target.value)}
+        placeholder="ingredients"
+      />
+      <input
+        value={imgUrl}
+        onChange={(e) => setImgUrl(e.target.value)}
+        placeholder="image url"
+      />
+      <button>{`${label}`} Recipe</button>
+    </form>
+  );
+}
--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -5,6 +5,11 @@ export default function RecipeItem({ recipe }) {
   return (
     <div className={styles.recipeItem}>
       <Link to={`/recipes/detail/${recipe.id}`}>
+        <img
+          src={recipe.img_url}
+          alt={`Image of ${recipe.title}`}
+          className={styles.image}
+        />
         <h3>{recipe.title}</h3>
       </Link>
     </div>
