0b15048811e4455b3b4500f02392269b4aa2ab40
Clayton Knapp <csjknapp@gmail.com>
2022-05-20 13:10:33 -0700

    added conditionally rendering Nav back to list button/link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,29 @@
 import useUser from '../hooks/useUser';
+import { Link, useLocation } from 'react-router-dom';
 
 
 export default function Header() {
   const { signOut, user } = useUser();
+  const { pathname } = useLocation();
+  console.log('pathname', pathname)
 
   return (
     <>
     {
-      user.email ? (<>
+      user.email && (
+        <>
           <p>You are signed in as {user.email}</p>
+          {
+            (pathname != '/recipes')
+              &&
+              <Link to='/recipes'>
+                <button>Go to Recipes List</button>
+              </Link>
+          }
           <button onClick={signOut}>log out</button>
-        </> )
-        : <></>
-      }
-      </>
+        </>
+      )    
+    }
+    </>
   )
 };
